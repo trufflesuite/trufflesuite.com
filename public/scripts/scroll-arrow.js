a328eb4ce69e047ae801ee2af266b24bbcb883c7
Josh Quintal <joshquin@gmail.com>
2017-04-25 15:04:17 -0400

    Various refinements, including sizing for short screens.

--- public/scripts/scroll-arrow.js
+++ public/scripts/scroll-arrow.js
@@ -2,11 +2,31 @@ $(function() {
 
   var scrollArrow = $('#scrollDown');
 
+  // Click to Scroll
+
   scrollArrow.click(function() {
     var navbarHeight = $('.navbar').outerHeight();
 
     $('html, body').animate({
       scrollTop: $('.BetaSignup').offset().top - navbarHeight
     }, 800);
+  });
+
+  // Scrolling Opacity
+
+  $(window).scroll(function() {
+    var windowScrollPos = $(this).scrollTop();
+
+    if (windowScrollPos > 0)
+    {
+      if (!scrollArrow.hasClass('scroll'))
+      {
+        return scrollArrow.addClass('scroll');
+      }
+
+      return;
+    }
+
+    return scrollArrow.removeClass('scroll');
   });
 });
