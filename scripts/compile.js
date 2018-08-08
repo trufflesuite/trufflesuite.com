3e6b823f8b06c2eb605daa5a5f9c7e6680d9facc
DiscRiskandBisque <joshquin@gmail.com>
2018-08-08 10:34:56 -0400

    Better bulid script location

--- scripts/compile.js
+++ scripts/compile.js
@@ -1,4 +1,4 @@
-var app = require("./metalsmith.js")
+var app = require("./../metalsmith.js");
 
 app.build(function(err) {      // build process
   if (err) throw err;       // error handling is required
