da330687369c06cd300c7aabf6b2e76cd169e390
Mike Seese <seesemichaelj@gmail.com>
2018-09-23 14:38:28 -0400

    Remove console log

--- src/js/scroll-arrow.js
+++ src/js/scroll-arrow.js
@@ -1,7 +1,5 @@
 $(function() {
   $(document).on('click', '#headerArrow', function(event) {
-    console.log('edge still sucks ass, just like IE!');
-
     $('html,body').animate({
       scrollTop: $('#contentBegins').offset().top - 60
     });
