d7faa9281f79cf7573dc9871bc5cd35723098ad2
Mike Seese <seesemichaelj@gmail.com>
2020-03-16 21:55:16 -0700

    add a query parameter to the css link

--- metalsmith.js
+++ metalsmith.js
@@ -40,7 +40,8 @@ function app(clean) {
     events: eventsData,
     pressReleases: pressReleasesData,
     staff: staffData,
-    tutorials: tutorialsData
+    tutorials: tutorialsData,
+    cssVersion: Date.now().toString(),
   })
   .use(discoverHelpers({
     directory: 'helpers',
