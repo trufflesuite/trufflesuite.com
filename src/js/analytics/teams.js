a16391e0d5bb6938f156ec78227f28a64ea0fc18
Josh Quintal <josh@trufflesuite.com>
2020-09-04 14:16:15 -0400

    https

--- src/js/analytics/teams.js
+++ src/js/analytics/teams.js
@@ -2,7 +2,7 @@
 function mixpanelTrackProxy(eventName, eventParams) {
   let httpRequest = new XMLHttpRequest();
 
-  httpRequest.open('POST', 'http://api-proxy/mixpanel/track');
+  httpRequest.open('POST', 'https://api-proxy/mixpanel/track');
   httpRequest.setRequestHeader("Content-Type", "application/json");
   
   httpRequest.onload = () => {
