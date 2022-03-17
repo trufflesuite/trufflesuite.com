92ca2227518ff80df6f6725b1b7a33aeb340c098
Minh Nguyen <minhnc@younetco.com>
2022-03-17 17:13:25 +0700

    Integration Contentful

--- main.py
+++ main.py
@@ -4,14 +4,115 @@ import base64
 
 import json
 import mkdocs
+import contentful
 
 import requests
 from requests.auth import HTTPBasicAuth
 
 from jinja2 import Template
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def nested_set(dic, keys, value):
+    for key in keys[:-1]:
+        dic = dic.setdefault(key, {})
+    dic[keys[-1]] = value
+
+def parse_url(path):
+    return os.path.join(path, '')
+
+def module_id_parse(id):
+    listMapping = {
+        'hero': 'hero',
+        'githubList': 'github-list',
+        'githubItem': 'github-list-item',
+        'button': 'button',
+        'logoList': 'logo-list',
+        'logoItem': 'logo-list-item',
+        'section': 'section',
+        'hubSpotForm': 'hubSpot-form',
+        'feature': 'feature',
+        'blog': 'blog-item',
+        'cardItem': 'card-item',
+        'richText': 'rich-text',
+        'gridListing': 'grid-listing',
+    }
+    return listMapping[id]
+
+def parse_asset_contentful(data, w = 1200):
+    if ("svg" in data.file['contentType']):
+        return data.url()
+    return data.url(w=w, fm='webp', q=100)
+
+def get_blogs_contentful(client, data):
+    blogs = client.entries({'limit': 500, 'content_type': 'blog', 'include': 10, 'order': '-fields.date,-sys.createdAt'})
+    data = data + blogs.items
+    size = len(data)
+    total = blogs.total
+    if(total > size):
+        return get_blogs_contentful(client, data)
+    return data
+
 def define_env(env):
     "Definition of the module"
+
+    contentful_space_id = os.getenv('CONTENTFUL_SPACE_ID')
+    contentful_token = os.getenv('CONTENTFUL_TOKEN')
+    env.conf['extra']['module_id_parse'] = module_id_parse;
+    env.conf['extra']['parse_asset_contentful'] = parse_asset_contentful;
+    env.conf['extra']['parse_url'] = parse_url;
+    client = contentful.Client(contentful_space_id, contentful_token)
+    site_dir = env.conf['docs_dir']
+
+    # Query all entry 'page'
+    entries = client.entries({'content_type': 'page', 'include': 10})
+    contentful_data = {}
+    # test = client.entry('1CgkzrVtPU7A4FbD6MfwwJ')
+    # from pprint import pprint
+    # pprint(test.date)
+    for entry in entries:
+        # dictModules = [{'template': module_id_parse(module.sys['content_type'].id), 'props': module} for module in entry.modules]
+        nested_set(contentful_data, [entry.slug], entry)
+        env.conf['extra']['contentful_page'] = contentful_data
+
+    #blog page
+    # contentful_entries_blogs = client.entries({'limit': 500, 'content_type': 'blog', 'include': 10, 'order': '-fields.date,-sys.createdAt'})
+    contentful_entries_blogs = get_blogs_contentful(client, [])
+    env.conf['extra']['contentful_blogs'] = contentful_entries_blogs
+
+    for blog in contentful_entries_blogs:
+        blog_dir = os.path.join(site_dir, 'blog', blog.slug)
+
+        if os.path.exists(blog_dir):
+            shutil.rmtree(blog_dir)
+            
+        os.makedirs(blog_dir)
+        file_path = os.path.join(blog_dir, 'index.md')
+
+        #content of blog detail
+        blog_detail = blog.detail
+
+        #heading detail and SEO title
+        blog_detail_title = blog.title
+        if hasattr(blog, 'detail_title'):
+            blog_detail_title = blog.detail_title
+
+        try:
+            markdown = blog_detail
+
+            with open('src/blog/blog.html.jinja2') as file_:
+                template = Template(file_.read())
+
+            outputText = template.render(blog=blog, content=markdown, title=blog_detail_title)
+
+            with open(file_path, 'w') as f:
+                f.write(outputText)
+
+        except Exception as ex:
+            print('error: ' + repr(ex))
 
     # boxes page
     data_file_object = open('src/boxes/data.json')
@@ -39,7 +140,6 @@ def define_env(env):
     if os.environ.get("LOCAL_BUILD"):
         env.conf['extra']['boxes'] = env.conf['extra']['boxes'][:6] 
 
-    site_dir = env.conf['docs_dir']
     username = os.environ.get("TRUFFLESUITE_COM_GH_API_USERNAME")
     key = os.environ.get("TRUFFLESUITE_COM_GH_API_KEY")
     for box in env.conf['extra']['boxes']:
@@ -70,18 +170,6 @@ def define_env(env):
 
 def on_pre_page_macros(env):
     "Pre-page actions"
-
-    # blog posts page
-    blog_file_object = open('src/blog/data.json')
-    posts = json.load(blog_file_object)
-    publishedposts = []
-    for key in posts.keys():
-        if posts[key]['published']:
-            entry = posts[key]
-            entry['route'] = key
-            publishedposts.append(entry)
-
-    env.conf['extra']['posts'] = publishedposts
 
     # guide page
     guide_file_object = open('src/guides/data.json')
